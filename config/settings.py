2095ad884ca03b7325baabf5a640eb9b7a643653
nematulloirgasev <akanematullo@gmail.com>
2024-07-20 17:25:09 +0500

    hhhhh

--- config/settings.py
+++ config/settings.py
@@ -77,14 +77,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -130,3 +129,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'avatars')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
