5d1e56b1bc00af02dafc20227cae2ce17c118d32
nematulloirgasev <akanematullo@gmail.com>
2024-08-07 18:08:52 +0500

    new changes

--- config/settings.py
+++ config/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
     }
 }
 # Password validation
@@ -130,3 +134,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'avatars')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+try:
+    from .settings_dev import *
+except ImportError:
+    pass
--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('catalog.urls', 'config'), namespace='catalog')),
     path("__reload__/", include("django_browser_reload.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
